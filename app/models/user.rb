8798fadf19260604f898203d117ce2f26cfae615
HoaiAn <hoaianr@gmail.com>
2015-01-07 15:31:02 +0700

    asd

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,18 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
+  validates :name, uniqueness: {case_sensitive: false}
   has_many :posts, dependent: :destroy
+  attr_accessor :login
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(name) = :value OR lower(email) = :value",{value: login.downcase}]).first
+    else
+      where(conditions).first
+    end
+  end
 end
