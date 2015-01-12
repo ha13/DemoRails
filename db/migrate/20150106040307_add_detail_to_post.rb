5893d09c805c2b984e96646589132054a1da6050
HoaiAn <hoaianr@gmail.com>
2015-01-12 11:09:53 +0700

    asdf

--- db/migrate/20150106040307_add_detail_to_post.rb
+++
@@ -1,8 +0,0 @@
-class AddDetailToPost < ActiveRecord::Migration
-  def up
-    add_column :posts, :slug, :string, index: true
-  end
-  def down
-    remove_column :posts, :slug, :string
-  end
-end
